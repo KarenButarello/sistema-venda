b664f61db3a5aa0f346c37635fccde088f1f5c7f
karen <karenkbutarello@gmail.com>
2024-01-30 22:53:31 -0300

    Criar teste Busca de vendedores com vendas

--- src/test/java/br/com/sistema/venda/controller/VendedorControllerTest.java
+++ src/test/java/br/com/sistema/venda/controller/VendedorControllerTest.java
@@ -1,9 +1,12 @@
 package br.com.sistema.venda.controller;
 
+import br.com.sistema.venda.dto.VendasVendedor;
+import br.com.sistema.venda.model.Venda;
 import br.com.sistema.venda.model.Vendedor;
 import br.com.sistema.venda.service.VendedorService;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +16,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -41,5 +48,27 @@ public class VendedorControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value("mateus"));
     }
+    @Test
+    public void deveMostrarVendasVendedor() throws Exception{
+        VendasVendedor vendasVendedor = new VendasVendedor();
+        BigDecimal bigDecimal = new BigDecimal(1);
 
+        vendasVendedor.setNome("joao");
+        vendasVendedor.setTotalVendas(31);
+        vendasVendedor.setMediaVenda(bigDecimal);
+
+        Mockito.when(vendedorService.buscaTodosVendedoresComVendas(LocalDate.of(2024,01,10), LocalDate.of(2024,02,10)))
+                .thenReturn(List.of(vendasVendedor));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/vendedor")
+                        .queryParam("inicio","2024-01-10")
+                        .queryParam("fim","2024-02-10")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].nome").value("joao"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].totalVendas").value(31))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].mediaVenda").value(1))
+                ;
+
+    }
 }
--- src/test/java/br/com/sistema/venda/service/VendedorServiceTest.java
+++ src/test/java/br/com/sistema/venda/service/VendedorServiceTest.java
@@ -1,9 +1,12 @@
 package br.com.sistema.venda.service;
 
 import br.com.sistema.venda.dto.NovoVendedor;
+import br.com.sistema.venda.dto.VendasVendedor;
 import br.com.sistema.venda.model.Venda;
 import br.com.sistema.venda.model.Vendedor;
+import br.com.sistema.venda.repository.VendaRepository;
 import br.com.sistema.venda.repository.VendedorRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -12,21 +15,51 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
 @ExtendWith(MockitoExtension.class)
 public class VendedorServiceTest {
 
     @Mock
     private VendedorRepository repository;
+    @Mock
+    private VendaService vendaService;
     @InjectMocks
     private VendedorService vendedorService;
 
     @Test
-    public void deveFazerVendedor(){
+    public void deveFazerVendedor() {
         Vendedor vendedor = new Vendedor();
         NovoVendedor novoVendedor = new NovoVendedor();
 
         Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(vendedor);
         vendedorService.salvarVendedor(novoVendedor);
         Mockito.verify(repository, Mockito.times(1)).save(ArgumentMatchers.any());
+    }
+
+    @Test
+    public void buscaTodosVendedoresComVendas() {
+        LocalDate inicio = LocalDate.of(2024, 01, 10);
+        LocalDate fim = LocalDate.of(2024, 02, 10);
+        Venda venda = new Venda();
+        Vendedor vendedor = new Vendedor();
+        vendedor.setId(1L);
+        vendedor.setNome("joao");
+
+
+        Mockito.when(repository.findAll()).thenReturn(List.of(vendedor));
+        Mockito.when(vendaService.findAllByVendedorIdAndDataVendaBetween(1L, inicio, fim))
+                .thenReturn(List.of(venda));
+
+
+
+        List<VendasVendedor> resultado = vendedorService.buscaTodosVendedoresComVendas(inicio, fim);
+
+        Assertions.assertEquals("joao", resultado.get(0).getNome());
+        Assertions.assertEquals(1L, (long) resultado.get(0).getTotalVendas());
+        Assertions.assertEquals(0.03d, resultado.get(0).getMediaVenda().doubleValue());
+
     }
 }
